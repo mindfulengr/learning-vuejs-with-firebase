b4ea534e2e3057eb5f7629bf4b624b217509ddf6
Paul Werner <mindfulengr@gmail.com>
2019-12-08 16:20:55 +0100

    add create post

--- vuegram/src/components/Dashboard.vue
+++ vuegram/src/components/Dashboard.vue
@@ -1,12 +1,61 @@
 <template>
-  <div>
-
+  <div id="dashboard">
+    <section>
+      <div class="col1">
+        <div class="profile">
+          <h5>{{ userProfile.name }} </h5>
+          <p>{{ userProfile.title }} </p>
+          <div class="create-post">
+            <label for="textarea">create a post</label>
+            <form @submit.prevent>
+              <textarea id="textarea" v-model.trim="post.content"/>
+              <button @click='createPost' :disabled="post.content === ''" class="button">post</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col2">
+        <div>
+          <p class="no-results">There are currently no posts</p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
+const fb = require('../firebaseConfig')
+
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  data () {
+    return {
+      post: {
+        content: ''
+      }
+    }
+  },
+  computed: {
+    ...mapState(['userProfile', 'currentUser'])
+  },
+  methods: {
+    createPost () {
+      fb.postsCollection.add({
+        createdOn: new Date(),
+        content: this.post.content,
+        userId: this.currentUser.uid,
+        userName: this.userProfile.name,
+        comments: 0,
+        likes: 0
+      }).then(() => {
+        this.post.content = ''
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 
