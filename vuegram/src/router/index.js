83b3a3048c7eec8a646385a237594866e4798f7a
Paul Werner <mindfulengr@gmail.com>
2019-12-08 12:34:24 +0100

    add routes

--- vuegram/src/router/index.js
+++ vuegram/src/router/index.js
@@ -1,13 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import firebase from 'firebase'
+
+import Login from '@/components/Login'
+import Dashboard from '@/components/Dashboard'
+import Settings from '@/components/Settings'
 
 Vue.use(VueRouter)
 
-const routes = [
-]
-
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  routes: [
+    {
+      path: '*',
+      redirect: '/dashboard'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: Settings,
+      meta: {
+        requiresAuth: true
+      }
+    }
+  ]
 })
 
 export default router
